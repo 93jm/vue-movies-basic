0ce828bb95f56cdee4e519b177b21580f5e76305
Evan <93_jm@naver.com>
2024-06-28 17:17:54 +0900

    done: vue3로 만든 간단한 영화 목록 앱

---
+++ src/assets/movies.js
@@ -0,0 +1,29 @@
+const data = [
+  {
+    id: 0,
+    title: "노량",
+    year: 2023,
+    category: "액션 드라마",
+    textRed: "color: red",
+    like: 0,
+    imgUrl: "./assets/노량_포스터.webp",
+  },
+  {
+    id: 1,
+    title: "매트릭스",
+    year: 1997,
+    category: "액션",
+    like: 0,
+    imgUrl: "./assets/매트릭스2_포스터.jpg",
+  },
+  {
+    id: 2,
+    title: "반지의 제왕",
+    year: 2003,
+    category: "모험, 판타지",
+    like: 0,
+    imgUrl: "./assets/반지의제왕_포스터.jpeg",
+  },
+];
+
+export default data;
